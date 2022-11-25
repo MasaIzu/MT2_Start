93698a995c4af3b521eb93ebe27631a6ec618512
masato izumida <Oioi_0126@outlook.com>
2022-11-25 14:19:04 +0900

    プロジェクト ファイルを追加します。

---
+++ Vector2.cpp
@@ -0,0 +1,106 @@
+#include "Vector2.h"
+#include <cmath>
+
+Vector2::Vector2() :x(0), y(0)
+{}
+
+Vector2::Vector2(float x, float y) : x(1.0f), y(3.0f)
+{
+
+}
+
+float Vector2::length() const
+{
+    return std::sqrt(x * x + y * y);
+}
+
+Vector2& Vector2::normalize()
+{
+    float len = length();
+    if (len != 0) {
+        return *this /= len;
+    }
+    return *this;
+}
+
+float Vector2::dot(const Vector2& v) const
+{
+    return x * v.x + y * v.y;
+}
+
+float Vector2::cross(const Vector2& v) const
+{
+    return x * v.x - y * v.y;;
+}
+
+Vector2 Vector2::operator+()const
+{
+
+    return *this;
+}
+
+Vector2 Vector2::operator-()const
+{
+    return Vector2(-*this);
+}
+
+Vector2& Vector2::operator+=(const Vector2& v)
+{
+    this->x += v.x;
+    this->y += v.y;
+
+    return *this;
+}
+
+Vector2& Vector2::operator-=(const Vector2& v)
+{
+    this->x -= v.x;
+    this->y -= v.y;
+
+    return *this;
+}
+
+Vector2& Vector2::operator+=(float s)
+{
+    this->x = s;
+    this->y = s;
+
+    return *this;
+}
+
+Vector2& Vector2::operator/=(float s)
+{
+    x /= s;
+    y /= s;
+    return *this;
+}
+
+
+const Vector2 operator + (const Vector2& v1, const Vector2& v2)
+{
+    Vector2 temp(v1);
+    return temp += v2;
+}
+
+const Vector2 operator - (const Vector2& v1, const Vector2& v2)
+{
+    Vector2 temp(v1);
+    return temp -= v2;
+}
+
+const Vector2 operator *(const Vector2& v, float s)
+{
+    Vector2 temp(v);
+    return temp * s;
+}
+
+const Vector2 operator *(float s, const Vector2& v)
+{
+    return v * s;
+}
+
+const Vector2 operator/(const Vector2& v, float s)
+{
+    Vector2 temp(v);
+    return temp / s;
+}
